a6e456ac6ae2b514bb63704322890e985809043c
jirum <monteclar.jerome19@gmail.com>
2022-08-03 17:10:45 +0800

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
 
   constraints(ClientDomainConstraint.new) do
-    devise_for :users, controllers: { sessions: 'users/sessions' }
+    devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+    root :to => 'home#index'
   end
 
   constraints(AdminDomainConstraint.new) do
     namespace :admin, path: '' do
-      devise_for :users, controllers: { sessions: 'admins/sessions' }
+      root :to => 'home#index'
+      devise_for :users, controllers: { sessions: 'admin/sessions' }
     end
   end
 end
