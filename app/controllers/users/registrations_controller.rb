6571cf749a87f1d03a526a1ac88c91d6737d73ab
jirum <monteclar.jerome19@gmail.com>
2022-08-08 14:48:57 +0800

    [Feat #4] User can update their profile without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,42 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def edit
+    @user = current_user
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def update
+    if params[:user][:current_password].present?
+      if current_user.update_with_password(password_params)
+        flash[:alert]="Updated successfully!"
+        sign_in @user, :bypass => true
+        redirect_to edit_user_registration_path
+      else
+        flash[:error] = 'Oh No! Something Went Wrong in password update field, Please Try Again.'
+        render :edit
+      end
+    else
+      if current_user.update(userinfo_params)
+        flash[:alert]="Updated successfully!"
+        redirect_to edit_user_registration_path
+      else
+        flash[:error] = 'Oh No! Something Went Wrong, Please Try Again.'
+        render :edit
+      end
+    end
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  private
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  def userinfo_params
+    params.require(:user).permit(:email, :username, :phone, :image)
+  end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def password_params
+    params.require(:user).permit(:password,:password_confirmation, :current_password)
+  end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :username, :image])
   end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
