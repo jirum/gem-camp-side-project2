f12ae6f2136420e6db74f7424a8c1f128aa9caf5
jirum <monteclar.jerome19@gmail.com>
2022-08-10 16:47:56 +0800

    [Feat #5] Get Opendata from PH Government

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'mysql2'
 gem 'devise'
 gem 'phonelib'
 gem 'carrierwave', '~> 2.0'
+gem 'rest-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/services/ph_location_service.rb
@@ -0,0 +1,66 @@
+class PhLocationService
+  attr_reader :url
+
+  def initialize
+    @url = 'https://psgc.gitlab.io/api'
+  end
+
+  def get_regions
+    response = RestClient.get("#{url}/regions")
+    regions = JSON.parse(response.body)
+    regions.each do |region|
+      Region.find_or_create_by(code: region['code'], name: region['name'], region_name: region['regionName'])
+    end
+  end
+
+  def get_provinces
+    response = RestClient.get("#{url}/provinces")
+    provinces = JSON.parse(response.body)
+    provinces.each do |province|
+      region = Region.find_by_code(province['regionCode'])
+      Province.find_or_create_by(code: province['code'], name: province['name'], region: region)
+    end
+
+    response = RestClient.get("#{url}/districts")
+    districts = JSON.parse(response.body)
+    districts.each do |district|
+      region = Region.find_by_code(district['regionCode'])
+      Province.find_or_create_by(code: district['code'], name: district['name'], region: region)
+    end
+  end
+
+  def get_cities
+    response = RestClient.get("#{url}/cities-municipalities")
+    cities = JSON.parse(response.body)
+    cities.each do |city|
+      if city['provinceCode']
+        province = Province.find_by_code(city['provinceCode'])
+        City.find_or_create_by(code: city['code'], name: city['name'], province: province)
+      elsif city['districtCode']
+        province = Province.find_by_code(city['districtCode'])
+        City.find_or_create_by(code: city['code'], name: city['name'], province: province)
+      else
+        if city['name'] == "City of Isabela"
+          province = Province.find_by_name('Basilan')
+          City.find_or_create_by(code: city['code'], name: city['name'], province: province)
+        elsif city['name'] == "City of Cotabato" #cotabato city
+          province = Province.find_by_name('Maguindanao')
+          City.find_or_create_by(code: city['code'], name: city['name'], province: province)
+        end
+      end
+    end
+  end
+
+  def get_barangays
+    response = RestClient.get("https://psgc.gitlab.io/api/barangays")
+    barangays = JSON.parse(response.body)
+    barangays.each do |barangay|
+      if barangay['cityCode']
+        city = City.find_by_code(barangay['cityCode'])
+      else
+        city = City.find_by_code(barangay['municipalityCode'])
+      end
+      Barangay.find_or_create_by(code: barangay["code"], name: barangay["name"], city: city)
+    end
+  end
+end
