b6c883a21464a826f3aa177aa1c62b06ab12fe2c
jirum <monteclar.jerome19@gmail.com>
2022-08-12 13:28:18 +0800

    [Feat #8] User increment children members

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
   validates :phone, phone: {allow_blank: true}
   has_many :addresses
-  belongs_to :parent, class_name: "User", optional: true
+  belongs_to :parent, class_name: "User", optional: true, counter_cache: :children_members
   has_many :children, class_name: "User", foreign_key: 'parent_id'
 
   mount_uploader :image, ImageUploader
