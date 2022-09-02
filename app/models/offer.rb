6b8fce35fb1d64f688026d3021ecec535b5f0eb9
jirum <monteclar.jerome19@gmail.com>
2022-09-02 09:46:13 +0800

    add numericality for amount and coin

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,7 @@
 class Offer < ApplicationRecord
-  validates :image, :name, :genre, :status, :amount, :coin, presence: true
+  validates :image, :name, :genre, :status, presence: true
+  validates :amount, :coin, numericality: { greater_than: 0 }
+
   enum status: [:active, :inactive]
   enum genre:[:one_time, :monthly, :weekly, :daily, :regular]
 
