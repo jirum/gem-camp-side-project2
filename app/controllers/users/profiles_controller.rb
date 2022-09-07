b475c90b69116e2f2912edb8678503d5d1dfba5f
jirum <monteclar.jerome19@gmail.com>
2022-09-07 10:13:35 +0800

    [Feat #23] Client Profile Page

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,5 +1,10 @@
 class Users::ProfilesController < ApplicationController
   before_action :authenticate_user!, only: :show
 
-  def show; end
+  def show
+    @orders = Order.where(user: current_user) if params[:report] == 'order' || params[:report].blank?
+    @bets = Bet.includes(:item).where(user: current_user) if params[:report] == 'bet'
+    @invitations = User.where(parent: current_user) if params[:report] == 'invite'
+    @winners = Winner.includes(:item, :bet).where(user: current_user) if params[:report] == 'winner'
+  end
 end
