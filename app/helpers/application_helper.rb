9670917ff6c3380d18fd6c39d361f76d223d97c3
jirum <monteclar.jerome19@gmail.com>
2022-09-14 14:29:39 +0800

    [Feat #34] Display banner and news_ticker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def concat_address(address)
     "#{address.street_address}, #{address.barangay.name}, #{address.city.name}, #{address.province.name}, #{address.region.name}"
   end
+
+  def banners
+    Banner.active.where("DATE(online_at) <= ?", Time.now).where("DATE(offline_at) > ?", Time.now)
+  end
+
+  def news_ticker
+    NewsTicker.active.limit(5)
+  end
+
 end
