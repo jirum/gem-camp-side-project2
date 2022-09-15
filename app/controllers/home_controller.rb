1feae597080d505576d66af30cc78634cfde65af
jirum <monteclar.jerome19@gmail.com>
2022-09-15 10:20:56 +0800

    [Feature 35] Client Homepage view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: :index
 
-  def index; end
+  def index
+    @winners = Winner.published.limit(5).order(id: :desc)
+    @items = Item.active.starting.limit(8)
+  end
 end
